ff7baa40576268666fed7e8cdbed40f5e52167e8
Carolyn Anderson <canders277@gmail.com>
2015-02-06 12:16:42 -0500

    created a function to generate index pages

--- data/main.py
+++ data/main.py
@@ -1,5 +1,6 @@
 from lxml import etree
 levels = 4  #depth of tree from root to level of page creation
+levelList = ["section", "subsection", "lesson", "unit"]
 unit_xsl_raw = etree.parse("unit.xsl")  #parse xsl document
 unit_xsl = etree.XSLT(unit_xsl_raw) #create an xslt transformation function
 which = [] #list of lists that associate each section with its title and page
@@ -18,55 +19,53 @@ def processUnit(sectionTitle, subsectionTitle, lessonTitle, unit, which):
   f.close()
   return which
 
-def processLesson(sectionTitle, subsectionTitle, lesson, which):
+def processLesson(sectionTitle, subsectionTitle, lesson, which, levelList):
   which[2][0] +=1  #increment which lesson is being processed
   title = lesson.xpath("lessontitle/text()")[0] #get lesson title
   which[2][1] = title
   units = lesson.xpath("unit")  #get units
   for unit in units: #call unit processing function on all units
     which = processUnit(sectionTitle, subsectionTitle, title, unit, which)
-  filename = "../%s.%s.%s.html" % (which[0][0], which[1][0], which[2][0])
-  which[2][2] = filename
-  f = open(filename, "w") #create new file
-  f.write("---\nlayout: blank\nsection: %s\nsubsection: %s\nlesson: %s\n---\n" % (sectionTitle, subsectionTitle, title)) #write jekyll layout markup
-  f.write("<h1>%s</h1>" % (which[2])) #write index of lesson to file
-  f.close()
+  createIndexFile(which, 0, levelList)
   return which
 
-def processSubsection(sectionTitle, subsection, which):
+def processSubsection(sectionTitle, subsection, which, levelList):
   which[1][0] +=1  #increment which subsection is being processed
   title = subsection.xpath("subsectitle/text()")[0] #get subsection title
   which[1][1] = title
   lessons = subsection.xpath("lesson")  #get lessons
   for lesson in lessons: #call lesson processing function on all lessons
-    which = processLesson(sectionTitle, title, lesson, which)
-  filename = "../%s.%s.html" % (which[0][0], which[1][0])
-  which[1][2] = filename
-  f = open(filename, "w") #create new file
-  f.write("---\nlayout: blank\nsection: %s\nsubsection: %s\n---\n" % (sectionTitle, title)) #write jekyll layout markup
-  f.write("<h1>%s</h1>" % (which[1])) #write index of subsection to file
-  f.close()
+    which = processLesson(sectionTitle, title, lesson, which, levelList)
+  createIndexFile(which, 1, levelList)
   return which
 
-def processSection(section, which):
+def processSection(section, which, levelList):
   which[0][0] +=1 #increment which section is being processed
   # xpath always returns a collection, even when it is singleton, thus the [0]
   title = section.xpath("sectiontitle/text()")[0] #get section title
   which[0][1] = title
   subsections = section.xpath("subsection") #get subsections
   for subsection in subsections: #call subsection processing function on all sections
-    which = processSubsection(title, subsection, which)
-  filename = "../%s.html" % (which[0][0])
-  which[0][2] = filename
-  f = open(filename, "w") #create new file
-  f.write("---\nlayout: blank\nsection: %s\n---\n" % (title)) #write jekyll layout markup
-  f.write("<h1>%s</h1>" % (which[0])) #write index of lesson to file
-  f.close()
+    which = processSubsection(title, subsection, which, levelList)
+  createIndexFile(which, 0, levelList)
   return which
 
+def createIndexFile(which, level, levelList):
+  filename = "../"
+  markup = "---\nlayout: blank\n"
+  for i in range(level):
+    filename = filename + "%s." % (which[i][0])
+    markup = markup + "%s: %s\n" % (levelList[i], which[i][1])
+  markup = markup + "---\n"
+  filename = filename + "html"
+  which[level-1][2] = filename
+  f = open(filename, "w") #create new file
+  f.write(markup) #write jekyll layout markup
+  f.write("<h1>%s</h1>" % (which[level-1])) #write index of level to file
+  f.close()
 
 doc = etree.parse("short_test.xml") #parse the xml doc
 sections = doc.xpath("/lessonset/section")  #get sections
 for section in sections: #call section processing function on all units
-  which = processSection(section, which)
+  which = processSection(section, which, levelList)
 print which
