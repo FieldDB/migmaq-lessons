8de8eff8f868e1c16174102c07dbe14d56633e84
Carolyn Anderson <canders277@gmail.com>
2015-02-10 14:53:25 -0500

    Figuring out XPath

--- data/main.py
+++ data/main.py
@@ -1,58 +1,76 @@
 from lxml import etree
 import pprint
 
-levels = 4  #depth of tree from root to level of page creation
+#levels = 4  #depth of tree from root to level of page creation
 levelList = ["section", "subsection", "lesson", "unit"]
 unit_xsl_raw = etree.parse("unit.xsl")  #parse xsl document
 unit_xsl = etree.XSLT(unit_xsl_raw) #create an xslt transformation function
-which = [] #list of lists that associate each section with its title and page
-for l in levelList:
-  which.append([])
+levels = {1:{}, 2:{}, 3:{}, 4:{}} #list of lists that associate each section with its title and page
 
-def trackLevel(level, title, filename, which):
-  if which[level] == None:
-    which[level] = []
-  newL = [title, filename]
-  which[level].append(newL)
-  return which
+def trackLevel(title, filename, which):
+  print which 
+  if len(which) == 0:
+    which.append("")
+    which.append("")
+    which.append([])
+  if len(which) == 2:
 
-def processUnit(level, parent, unit, which):
+    which.append([])
+  children = which[2]
+  newL = [title, filename, None]
+  which[2].append(newL)
+  return children[len(children)-1]
+
+def processUnit(levels, level, unit):
   title = unit.xpath("unittitle/text()")[0]
-  filename = "../%s.%s.%s.%s.html" % (len(which[0]), len(which[1]), len(which[2]), len(which[3]))
+  l = levels[level]
+  parent = l[unit][0][0]
+  number = l[unit][0][1]
+  lesson = unit.xpath("..")[0]
+  lesson_title = lesson.xpath("lessontitle/text()")[0]
+  subsection = lesson.xpath("..")[0]
+  subsection_title = subsection.xpath("subsectitle/text()")[0]
+  section = subsection.xpath("..")[0]
+  section_title = section.xpath("sectiontitle/text()")[0]
+  filename = "../%s.%s.%s.%s.html" % (1, 2, 3, number)
+  l[unit] = [(parent, filename, title)]
   f = open(filename, "w") #create new file
-  f.write("---\nlayout: blank\nsection: %s\nsubsection: %s\nlesson: %s\nunit: %s\n---\n" % ("1", "2", "3", "4")) #(sectionTitle, subsectionTitle, lessonTitle, title)) #write jekyll layout markup
+  f.write("---\nlayout: blank\nsection: %s\nsubsection: %s\nlesson: %s\nunit: %s\n---\n" % (section_title, subsection_title, lesson_title, title)) #(sectionTitle, subsectionTitle, lessonTitle, title)) #write jekyll layout markup
   f.write(str(unit_xsl(unit))) #apply xslt transformation to unit data and write to file
   f.close()
-  which = trackLevel(level, title, filename, which)
-  return which
 
-def processLesson(level, lesson, which, levelList):
+def processLesson(levels, level, lesson):
   title = lesson.xpath("lessontitle/text()")[0] #get lesson title
+  l = levels[level]
+  parent = l[lesson][0]
+  l[lesson] = [(parent, "", title)]
   units = lesson.xpath("unit")  #get units
-  which = trackLevel(level, title, "", which)
-  which = createIndexFile(which, level, levelList)
   for unit in units: #call unit processing function on all units
-    which = processUnit(level-1, unit, which)
-  return which
+    l[lesson].append(unit)
+    levels[level+1][unit] = [(lesson, len(l[lesson])-1, "", "")]
+    processUnit(levels, level+1, unit)
 
-def processSubsection(level, subsection, which, levelList):
+def processSubsection(levels, level, subsection):
   title = subsection.xpath("subsectitle/text()")[0] #get subsection title
+  l = levels[level]
+  parent = l[subsection][0]
+  l[subsection] = [(parent, "", title)]
   lessons = subsection.xpath("lesson")  #get lessons
-  which = trackLevel(level, title, "", which)
-  which = createIndexFile(which, level, levelList)
   for lesson in lessons: #call lesson processing function on all lessons
-    which = processLesson(level-1, lesson, which, levelList)
-  return which
+    l[subsection].append(lesson)
+    levels[level+1][lesson] = [(subsection, len(l[subsection])-1, "", "")]
+    processLesson(levels, level+1, lesson)
 
-def processSection(level, section, which, levelList):
+def processSection(levels, level, section):
   # xpath always returns a collection, even when it is singleton, thus the [0]
   title = section.xpath("sectiontitle/text()")[0] #get section title
-  subsections = section.xpath("subsection") #get subsections 
-  which = trackLevel(level, title, "", which)
-  which = createIndexFile(which, level, levelList)
+  l = levels[level]
+  l[section] = [("", title)]
+  subsections = section.xpath("subsection") #get subsections
   for subsection in subsections: #call subsection processing function on all sections
-    which = processSubsection(level-1, subsection, which, levelList)
-  return which
+    l[section].append(subsection)
+    levels[level+1][subsection] = [(section, len(l[section])-1, "", "")]
+    processSubsection(levels, level+1, subsection)
 
 def createIndexFile(which, level, levelList):
   filename = "../"
@@ -79,14 +97,6 @@ def createIndexFile(which, level, levelList):
 doc = etree.parse("short_test.xml") #parse the xml doc
 sections = doc.xpath("/lessonset/section")  #get sections
 for section in sections: #call section processing function on all units
-  which = processSection(0, section, which, levelList)
-pprint.pprint(which)
-for o in which:
-  print "First level"
-  print o 
-  print "-------"
-  for t in o:
-    print "Second level"
-    print t
-    print "-------"
+  w = processSection(levels, 1, section)
+pprint.pprint(levels)
 
