8518cf9bbcde268eb68a6da14c9ea2db81cb6325
Carolyn Anderson <canders277@gmail.com>
2015-02-24 14:31:33 -0500

    Added configuration file and Jekyll collections

--- data/big.py
+++ data/big.py
@@ -17,8 +17,8 @@ Author: Carolyn Anderson          Last Modified: 2/13/2015
 """
 
 #Create xsl transformation functions
-unit_xsl_raw = etree.parse("lesson_big.xsl")  #Displays data from Unit down
-unit_xsl = etree.XSLT(unit_xsl_raw) 
+lesson_xsl_raw = etree.parse("lesson_big.xsl")  #Displays data from Unit down
+lesson_xsl = etree.XSLT(lesson_xsl_raw) 
 index_xsl_raw = etree.parse("index_big.xsl")  #Makes an index of child files
 index_xsl = etree.XSLT(index_xsl_raw) 
 intro_xsl_raw = etree.parse("intro_big.xsl")  #Creates intro page
@@ -26,22 +26,22 @@ intro_xsl = etree.XSLT(intro_xsl_raw)
 
 def processLesson(level, lesson):
   #Rransforms to all units--- creates webpages for each displaying dialogues
-  filename = createFilename(lesson, level)#create filename
+  filename = "../lessons/" + createFilename(lesson, level)#create filename
   f = open(filename, "w") #create new file
   f.write(getMarkup(lesson, level)) #write Jekyll markup to top
-  f.write(str(unit_xsl(lesson))) #apply xslt transformation to unit data and write to file
+  f.write(str(lesson_xsl(lesson))) #apply xslt transformation to unit data and write to file
   f.close()
 
 def processUnit(level, unit):
   #Transforms all lessons and creates an index file for each showing child files
-  createIndexFile(unit, level)
+  createIndexFile("units", unit, level)
   lessons = unit.xpath("lesson")  #get units
   for lesson in lessons: #call unit processing function on all units
       processLesson(level+1, lesson)
 
 def processSection(level, section):
   #Transforms all sections and creates an index file for each showing child files  
-  createIndexFile(section, level)
+  createIndexFile("sections", section, level)
   units = section.xpath("unit") #get subsections
   for unit in units: #call subsection processing function on all sections
     processUnit(level+1, unit)
@@ -56,9 +56,9 @@ def processLessonSet(level, lessonset):
   for section in sections: #call section processing function on all units
     processSection(level+1, section)
 
-def createIndexFile(current, level):
+def createIndexFile(location, current, level):
   #Creates an index file displaying titles of all child files and linking to them
-  filename = createFilename(current,level)#create filename
+  filename = "../" + location + "/" + createFilename(current,level)#create filename
   f = open(filename, "w") #create new file
   f.write(getMarkup(current, level))#write Jekyll markup to top
   f.write(str(index_xsl(current))) #apply xslt transformation to generate an index and write to file
@@ -97,7 +97,7 @@ def createFilename(current, level):
     index = getIndex(current) #get index of parent node
     filename = str(index) + "." + filename #add parent node index to filename
     current = parent #set current node to parent to move up a level
-  filename = "../" + filename + "html"
+  filename = filename + "html"
   return filename
 
 def getIndex(current):
--- data/lesson_big.xsl
+++ data/lesson_big.xsl
@@ -10,17 +10,20 @@
         <div class="container">
       	<h1><xsl:value-of select="title"/></h1><!--Display unit title at top of page-->
         <img class="img-responsive thumbnail" src="../img/wikiimage.png" alt="Trees" style="max-width: 75%"></img><!--Display unit img-->
-        <p><xsl:value-of select="designnote"/></p><!--Display unit intro as a paragraph-->
+        <p><xsl:value-of select="designnote"/></p><!--Display design note as a paragraph-->
         <xsl:for-each select="dialog"><!--Iterate through all child dialogs-->
-        	<p>
-            <xsl:for-each select="line"><!--Iterate through all the lines in each dialog-->
-        			<h2><xsl:value-of select="migmaq"/></h2><!--Display Mi'gmaq-->
-        			<h2><xsl:value-of select="english"/></h2><!--Display English-->
-              <xsl:for-each select="soundfile">
-                <embed src="{.}.wav" autostart="no" height="12"></embed>
-              </xsl:for-each>
-        		</xsl:for-each>    		
-        	</p>  	
+          <xsl:for-each select="line"><!--Iterate through all the lines in each dialog-->
+            <div class="media">
+              <div class="media-left">
+                <embed class="media-object" src="soundfile/{.}.wav" autostart="no" height="12"></embed>
+                <!--Make a media object with the audio file-->
+              </div>
+              <div class="media-body">
+                <h2 class="media-heading"><xsl:value-of select="migmaq"/></h2><!--Display Mi'gmaq-->
+                <h4 class="media-heading"><xsl:value-of select="english"/></h4><!--Display English-->
+              </div>
+            </div>
+          </xsl:for-each>
         </xsl:for-each>
         <p>
           <xsl:value-of select="info"/><!--Display any info associated with the unit-->
