2439d97924124cbeac6b72c0588aa4eb7c0324bb
Carolyn Anderson <canders277@gmail.com>
2015-03-16 11:40:41 -0400

    Sidenav files commented, test files deleted

--- data/sidenav.py
+++ data/sidenav.py
@@ -2,7 +2,11 @@ from lxml import etree
 import sys
 
 """
-
+A script to generate a sidenav with nested table of contents, given an xml tree with 
+levels of lessonset, section, unit, and lesson (required command-line argument).
+Uses sidenav.xsl to transform the tree and generate the html, and writes the output to 
+sidenav.html in the _includes folder.
+Author: Carolyn Anderson 		Last modified: 3/16/2015
 """
 
 #Create xsl transformation functions
@@ -11,6 +15,6 @@ side_xsl = etree.XSLT(side_xsl_raw)
 doc = etree.parse(sys.argv[1]) #parse the xml doc
 
 lessonset = doc.xpath("/lessonset")[0]#get lessonset
-f = open("../_includes/sidenav_t.html", "w") #create new file in _includes folder
+f = open("../_includes/sidenav.html", "w") #create new file in _includes folder
 f.write(str(side_xsl(lessonset))) #apply xslt transformation and write to file
 f.close()
